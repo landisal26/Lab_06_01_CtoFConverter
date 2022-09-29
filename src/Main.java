bf9a523430ce108b38bd4ae65733406e9424d9ba
landisal <landisal@mail.uc.edu>
2022-09-29 12:55:53 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+         double Celsius;
+         double Far;
+         String Trash = "";
+
+
+        Celsius = in.nextInt();
+         in.nextLine();
+         System.out.println("Enter the temperature in celsius ");
+
+         Far = Celsius * 1.8 + 32;
+
+        System.out.println("\n Your conversion is: " +Far);
+
+
+        if(in.hasNextInt()) {
+            Celsius = in.nextInt();
+            in.nextLine();
+            System.out.println("The Temp is " + Far);
+        }
+
+        else {
+
+
+            Trash = in.nextLine();
+            System.out.println(Trash + " is not a valid number");
+        }
     }
 }
